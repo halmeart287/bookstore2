6782791fd581d97ae162d0028ed6bc73321a70e2
Arttu Halmetoja <bgh287@myy.haaga-helia.fi>
2020-04-06 13:47:46 +0300

    Added Category Entity

--- src/main/java/hh/swd20/bookstore2/Bookstore2Application.java
+++ src/main/java/hh/swd20/bookstore2/Bookstore2Application.java
@@ -9,28 +9,46 @@ import org.springframework.context.annotation.Bean;
 
 import hh.swd20.bookstore2.domain.Book;
 import hh.swd20.bookstore2.domain.BookRepository;
+import hh.swd20.bookstore2.domain.Category;
+import hh.swd20.bookstore2.domain.CategoryRepository;
 
 @SpringBootApplication
 public class Bookstore2Application {
-	
+
 	private static final Logger log = LoggerFactory.getLogger(Bookstore2Application.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(Bookstore2Application.class, args);
 	}
-	
+
 	@Bean
 	public CommandLineRunner bookDemo(BookRepository bookRepository) {
 		return (args) -> {
 			log.info("save a few books");
 			bookRepository.save(new Book("Taru Sormusten Herrasta", "J.R.R. Tolkien", "123-123-123AB", 1982, 59.99));
 			bookRepository.save(new Book("Mateki - The Magic Flute", "Yoshitaka Amano", "321-321-321BA", 1999, 79.49));
-			
+
 			log.info("fetch all books");
 			for (Book book : bookRepository.findAll()) {
 				log.info(book.toString());
 			}
 		};
+	}
+
+	@Bean
+	public CommandLineRunner categoryDemo(CategoryRepository categoryRepository) {
+		return (args) -> {
+			log.info("save some categories");
+			categoryRepository.save(new Category("Sci-Fi"));
+			categoryRepository.save(new Category("Fantasy"));
+			categoryRepository.save(new Category("Artistic"));
+
+			log.info("fetch all categories");
+			for (Category category : categoryRepository.findAll()) {
+				log.info(category.toString());
+			}
+		};
+
 	}
 
 }
